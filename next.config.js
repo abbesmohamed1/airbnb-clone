222d3c7152248bad2a0a0ce8c24c207f75ca3b0c
Mohamed Abbes <mohamed.abbes@esprit.tn>
2021-08-27 00:47:20 -0700

    last update

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 module.exports ={
     images: {
         domains: ['links.papareact.com'],
+    },
+    env:{
+        mapbox_key: "pk.eyJ1IjoibW9oYW1lZGFiYmVzIiwiYSI6ImNrZm4wbjdldDAwOTAzMnRhZ2R6d3V6NG0ifQ.2dMYmgA5CqYYZF_TUsL5Zw",
     }
 }
--- pages/search.js
+++ pages/search.js
@@ -3,6 +3,7 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import {format} from "date-fns";
 import InfoCard from "../components/InfoCard";
+import Map from "../components/Map";
 function Search({searchResult}) {
 
     const router = useRouter();
@@ -45,6 +46,10 @@ function Search({searchResult}) {
                   ))}
                   </div>
 
+              </section>
+
+              <section className="hidden xl:inline-flex xl:min-w-[600px] ">
+                  <Map searchResults= {searchResult}/>
               </section>
           </main>
 
